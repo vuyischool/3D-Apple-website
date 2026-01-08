a4ca5d6427d35e24030240639327747db121c73f
Vuyisile Nqono <vuyischoolac@gmail.com>
2026-01-08 22:00:48 +0200

    Implement 3D Product Viewer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import gsap from "gsap"
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
+import { ScrollTrigger } from "gsap/all"
 
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
